0443235d71d209d72258764ee76fa5ca59a325ab
GustavonBrito <gustavoserafio@gmail.com>
2025-05-05 01:37:33 -0300

    feat: initializing validations from user

---
+++ philo/utils_validations.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_validations.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/04 22:40:40 by gustavo-lin       #+#    #+#             */
+/*   Updated: 2025/05/05 01:23:20 by gustavo-lin      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_atol(const char *nptr)
+{
+	int		index;
+	int		sign;
+	long	result;
+
+	index = 0;
+	sign = 1;
+	result = 0;
+	while ((nptr[index] >= 9 && nptr[index] <= 13) || nptr[index] == ' ')
+		index++;
+	if (nptr[index] == '+')
+		index++;
+	else if (nptr[index] == '-')
+	{
+		sign = -1 * sign;
+		index++;
+	}
+	while (nptr[index] >= 48 && nptr[index] <= 57)
+	{
+		result = 10 * result + (nptr[index] - '0');
+		index++;
+	}
+	if (!(result >= 0 && result <= 2147483647))
+		return (-1);
+	return (result * sign);
+}
+
+int valid_just_numbers(char **argv)
+{
+	int i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (!(argv[i] >= 48 && argv[i] <= 57))
+			return (-1);
+	}
+	return (1);
+}
