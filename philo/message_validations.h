0fb620498335af47a23c3f96c93711faddfa876f
GustavonBrito <gustavoserafio@gmail.com>
2025-05-05 01:43:30 -0300

    adding headers

---
+++ philo/message_validations.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   message_validations.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/04 23:30:45 by gustavo-lin       #+#    #+#             */
+/*   Updated: 2025/05/04 23:31:46 by gustavo-lin      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MESSAGE_VALIDATIONS
+#define MESSAGE_VALIDATIONS
+
+#define MAX_PHILO_NUMS 200
+
+#endif
--- philo/philo.h
+++ philo/philo.h
@@ -6,35 +6,46 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 10:33:22 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/05/02 02:58:04 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/05/05 01:23:28 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
 
-#include <sys/time.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
 
-typedef struct s_timeval {
-    time_t      tv_sec;     // Segundos desde 1º de janeiro de 1970 (Epoch)
-    suseconds_t tv_usec;    // Microssegundos
-} t_timeval;
+int init_validations(int argc, char **argv);
+int	ft_atol(const char *nptr);
+int valid_just_numbers(char **argv);
 
 typedef struct s_rules
 {
-	int	philo_num;
-	int	times_to_die;
-	int	times_to_eat;
-	int	times_to_sleep;
-}	t_rules;
+	int             philo_num;      // Número de filósofos
+	long            time_to_die;    // Tempo máximo SEM comer antes de morrer (em ms)
+	long            time_to_eat;    // Tempo para comer (em ms)
+	long            time_to_sleep;  // Tempo para dormir (em ms)
+	int             must_eat;       // (Opcional) Quantas vezes cada filósofo deve comer
+	int             someone_died;   // Flag para parar tudo quando alguém morrer
 
+	long            start_time;     // Marca o início da simulação (em ms)
+	pthread_mutex_t *forks;         // Array de mutexes: cada garfo é um mutex
+	pthread_mutex_t m_write;        // Mutex para garantir que prints não se misturem
+}	t_rules;
 
 typedef struct s_philo
 {
-	int 		id;
-	int 		current_state;
-	t_timeval	*last_meal_time;
-	t_rules		*rules;
+	int             id;             // ID único do filósofo (de 1 a n)
+	int             meals;          // Quantas vezes este filósofo já comeu
+	long            last_meal;      // Timestamp da última refeição
+	pthread_t       thread;         // A thread do filósofo
+	pthread_mutex_t *left_fork;     // Ponteiro para o garfo da esquerda (mutex)
+	pthread_mutex_t *right_fork;    // Ponteiro para o garfo da direita (mutex)
+	t_rules         *rules;         // Ponteiro para t_rules
 }	t_philo;
 
 #endif
