2d62a389cfcb6b68617643ca775a6cb4a0ce6d96
GustavonBrito <gustavoserafio@gmail.com>
2025-05-07 00:07:11 -0300

    calling function to parse datas

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 22:09:34 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/05/04 22:35:08 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/05/06 23:58:16 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 
 int main(int argc, char **argv)
 {
-	if (init_validations(argc, argv) == -1)
+	t_rules *rules;
+
+	rules = (t_rules *)malloc(sizeof(t_rules));
+	if (!rules)
 		return (0);
+	if (init_validations(argc, argv) == -1)
+	{
+		free(rules);
+		printf("Usage: ./philo [1-200 philosophers] [time_to_die] [time_to_eat] [time_to_sleep] [optional: times_each_philo_must_eat]\n");
+		return (0);
+	}
+	parser(argc, argv, rules);
+	
 }
