188a8c2c810e2e335dd2f733d09af5075ffe646f
GustavonBrito <gustavoserafio@gmail.com>
2025-05-12 00:13:13 -0300

    feat: archive to do philo routine

---
+++ philo/philo_routine.c
@@ -0,0 +1,13 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_routine.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 00:08:24 by gustavo-lin       #+#    #+#             */
+/*   Updated: 2025/05/12 00:08:51 by gustavo-lin      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// arquivo para criar uma funcao *void que faz toda a rotina de um filosofo, pensa, come, dorme e talvez morre.s
