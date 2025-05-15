57a5e1b0ccdbff7d96b3ba37698f4875fdd4b58e
GustavonBrito <gustavoserafio@gmail.com>
2025-05-14 21:59:09 -0300

    feat: ensuring inputs are validated

---
+++ input_validations.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_validations.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 10:33:23 by gustavo-lin       #+#    #+#             */
+/*   Updated: 2025/05/12 22:02:20 by gustavo-lin      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+#include "message_validations.h"
+
+int init_validations(int argc, char **argv)
+{
+	int i;
+	
+	i = 1;
+	if (argc != 6 && argc != 5)
+		return (-1);
+	if (valid_just_numbers(argv[i]) <= 0 || ft_atol(argv[i]) > MAX_PHILO_NUMS)
+		return (-1);
+	i++;
+	while (i < argc)
+	{
+		if (valid_just_numbers(argv[i]) <= 0 || ft_atol(argv[i]) == -1)
+			return (-1);
+		i++;
+	}
+	return (0);
+}
