cca6bbc66d338b461cbbb22332c783622965c930
GustavonBrito <gustavoserafio@gmail.com>
2025-05-22 11:16:08 -0300

    feat: almost finishing philos routines, has to implement a way when a philo dies finishes instatly

--- philo/actions.c
+++ philo/actions.c
@@ -6,15 +6,15 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 15:49:01 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/05/20 23:37:02 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/05/22 11:12:25 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	die(t_philo philo);
 void	eat(t_philo *philo);
 void	sleep_philo(t_philo *philo);
-void	die(t_philo *philo);
 void	think(t_philo *philo);
 void	*dead_scan(void *arg);
 
@@ -27,7 +27,6 @@ void think(t_philo *philo)
 
 void eat(t_philo *philo)
 {
-	// if (philo->last_meal - get_timestamp() >= philo->rules->time_to_die)
 	if (philo->id % 2 == 0)
 	{
 		pthread_mutex_lock(philo->right_fork);
@@ -68,30 +67,32 @@ void sleep_philo(t_philo *philo)
 	usleep(philo->rules->time_to_sleep * 1000);
 }
 
-void die(t_philo *philo)
+void die(t_philo philo)
 {	
-	pthread_mutex_lock(&philo->rules->m_write);
-	printf("%ld %d: died\n", get_timestamp() - philo->rules->start_time, philo->id);
-	pthread_mutex_unlock(&philo->rules->m_write);
+	pthread_mutex_lock(&philo.rules->m_write);
+	printf("%ld %d died\n", get_timestamp() - philo.rules->start_time, philo.id);
+	pthread_mutex_unlock(&philo.rules->m_write);
+	pthread_mutex_lock(&philo.rules->dead_philo);
+	philo.rules->someone_died = 1;
+	pthread_mutex_unlock(&philo.rules->dead_philo);
 }
 
 void *dead_scan(void *arg)
-{	
+{
+	int i;
 	t_philo *philo = (t_philo *)arg;
-	//pthread_mutex_init(philo->rules.dead_philo, NULL);
-	
+	t_rules *rules = philo[0].rules;
+	i = 0;
 	while (1)
 	{
-		sleep(5);
-		printf("%ld\n", philo[2].last_meal);
-		// if (philo->last_meal - get_timestamp() >= philo->rules->time_to_die)
-		// {
-		// 	//die(philo);
-		// 	//pthread_mutex_lock(philo->rules->dead_philo);
-		// 	//philo->rules->someone_died = 1;
-		// 	//pthread_mutex_unlock(philo->rules->dead_philo);
-		// 	return (NULL);
-		// 	//Verificar melhor retorno depois e tambem todos os frees que é preciso fazer.
-		// }
+		if (rules->someone_died)
+			return NULL;
+		if (i >= rules->philo_num)
+			i = 0;
+		if ((get_timestamp() - philo[i].last_meal) > rules->time_to_die && philo[i].last_meal != 0)
+			return (die(philo[i]), NULL);
+		i++;
+		usleep(1000);
 	}
+	return (NULL);
 }
