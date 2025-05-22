71073f2678b3c53c724810cd9cffb9e394be718e
GustavonBrito <gustavoserafio@gmail.com>
2025-05-22 15:33:20 -0300

    feat: dead_scan moved to utils archive

--- philo/actions.c
+++ philo/actions.c
@@ -6,7 +6,7 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 15:49:01 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/05/22 11:12:25 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/05/22 15:32:48 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	die(t_philo philo);
 void	eat(t_philo *philo);
 void	sleep_philo(t_philo *philo);
 void	think(t_philo *philo);
-void	*dead_scan(void *arg);
 
 void think(t_philo *philo)
 {
@@ -27,6 +26,8 @@ void think(t_philo *philo)
 
 void eat(t_philo *philo)
 {
+	if (philo->rules->must_eat != 0 && philo->rules->must_eat <= philo->meals)
+		return ;
 	if (philo->id % 2 == 0)
 	{
 		pthread_mutex_lock(philo->right_fork);
@@ -75,24 +76,4 @@ void die(t_philo philo)
 	pthread_mutex_lock(&philo.rules->dead_philo);
 	philo.rules->someone_died = 1;
 	pthread_mutex_unlock(&philo.rules->dead_philo);
-}
-
-void *dead_scan(void *arg)
-{
-	int i;
-	t_philo *philo = (t_philo *)arg;
-	t_rules *rules = philo[0].rules;
-	i = 0;
-	while (1)
-	{
-		if (rules->someone_died)
-			return NULL;
-		if (i >= rules->philo_num)
-			i = 0;
-		if ((get_timestamp() - philo[i].last_meal) > rules->time_to_die && philo[i].last_meal != 0)
-			return (die(philo[i]), NULL);
-		i++;
-		usleep(1000);
-	}
-	return (NULL);
 }
