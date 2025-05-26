2c0c560795956cb7e57175d4404eb2701d11cde4
GustavonBrito <gustavoserafio@gmail.com>
2025-05-25 21:24:16 -0300

    feat: updating actions spliting eat function to call handle_forks()

--- philo/actions.c
+++ philo/actions.c
@@ -6,7 +6,7 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 15:49:01 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/05/22 23:17:19 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/05/25 19:15:52 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,49 +17,24 @@ void	eat(t_philo *philo);
 void	sleep_philo(t_philo *philo);
 void	think(t_philo *philo);
 
-void think(t_philo *philo)
+void	think(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->rules->m_write);
 	if (philo->rules->someone_died != 1)
-		printf("%ld %d is thinking\n", get_timestamp() - philo->rules->start_time, philo->id);
+		printf("%ld %d is thinking\n", get_timestamp()
+			- philo->rules->start_time, philo->id);
 	pthread_mutex_unlock(&philo->rules->m_write);
-};
+}
 
-void eat(t_philo *philo)
+void	eat(t_philo *philo)
 {
 	if (philo->rules->must_eat != 0 && philo->rules->must_eat <= philo->meals)
 		return ;
-	if (philo->id % 2 == 0)
-	{
-		pthread_mutex_lock(philo->right_fork);
-		pthread_mutex_lock(&philo->rules->m_write);
-		if (philo->rules->someone_died != 1)
-			printf("%ld %d has taken a fork\n", get_timestamp() - philo->rules->start_time, philo->id);
-		pthread_mutex_unlock(&philo->rules->m_write);
-		pthread_mutex_lock(philo->left_fork);
-		pthread_mutex_lock(&philo->rules->m_write);
-		if (philo->rules->someone_died != 1)
-			printf("%ld %d has taken a fork\n", get_timestamp() - philo->rules->start_time, philo->id);
-		pthread_mutex_unlock(&philo->rules->m_write);
-	}
-	else
-	{
-		pthread_mutex_lock(philo->left_fork);
-		pthread_mutex_lock(&philo->rules->m_write);
-		if (philo->rules->someone_died != 1)
-			printf("%ld %d has taken a fork\n", get_timestamp() - philo->rules->start_time, philo->id);
-		if (philo->rules->philo_num == 1)
-			usleep(philo->rules->time_to_die * 1000);
-		pthread_mutex_unlock(&philo->rules->m_write);
-		pthread_mutex_lock(philo->right_fork);
-		pthread_mutex_lock(&philo->rules->m_write);
-		if (philo->rules->someone_died != 1)
-			printf("%ld %d has taken a fork\n", get_timestamp() - philo->rules->start_time, philo->id);
-		pthread_mutex_unlock(&philo->rules->m_write);
-	}
+	handle_forks(philo);
 	pthread_mutex_lock(&philo->rules->m_write);
 	if (philo->rules->someone_died != 1)
-		printf("%ld %d is eating\n", get_timestamp() - philo->rules->start_time, philo->id);
+		printf("%ld %d is eating\n", get_timestamp() - philo->rules->start_time,
+			philo->id);
 	pthread_mutex_unlock(&philo->rules->m_write);
 	usleep(philo->rules->time_to_eat * 1000);
 	philo->last_meal = get_timestamp();
@@ -68,19 +43,21 @@ void eat(t_philo *philo)
 	pthread_mutex_unlock(philo->left_fork);
 }
 
-void sleep_philo(t_philo *philo)
+void	sleep_philo(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->rules->m_write);
 	if (philo->rules->someone_died != 1)
-		printf("%ld %d is sleeping\n", get_timestamp() - philo->rules->start_time, philo->id);
+		printf("%ld %d is sleeping\n", get_timestamp()
+			- philo->rules->start_time, philo->id);
 	pthread_mutex_unlock(&philo->rules->m_write);
 	usleep(philo->rules->time_to_sleep * 1000);
 }
 
-void die(t_philo philo)
-{	
+void	die(t_philo philo)
+{
 	pthread_mutex_lock(&philo.rules->m_write);
-	printf("%ld %d died\n", get_timestamp() - philo.rules->start_time, philo.id);
+	printf("%ld %d died\n", get_timestamp() - philo.rules->start_time,
+		philo.id);
 	pthread_mutex_unlock(&philo.rules->m_write);
 	pthread_mutex_lock(&philo.rules->dead_philo);
 	philo.rules->someone_died = 1;
