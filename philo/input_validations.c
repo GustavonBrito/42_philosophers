c5a6e84b8e1c24ef4157cebbc48a8f42c3bc7fe0
GustavonBrito <gustavoserafio@gmail.com>
2025-05-25 21:36:32 -0300

    added print_error_message to adapt to norm

--- philo/input_validations.c
+++ philo/input_validations.c
@@ -6,30 +6,34 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 10:33:23 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/05/22 19:55:45 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/05/25 18:43:01 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-#include "message_validations.h"
 
-int init_validations(int argc, char **argv)
+void	print_error_message(void)
 {
-	int i;
-	
+	printf("Usage: ./philo [1-200 philosophers] \
+[time_to_die] [time_to_eat] [time_to_sleep] \
+[optional: times_each_philo_must_eat] \
+\nAll values must be positive\n");
+}
+
+int	init_validations(int argc, char **argv)
+{
+	int	i;
+
 	i = 1;
-	
 	if (argc != 6 && argc != 5)
-		return (-1);
-	// Valida numero de filosofos
-	if (valid_just_numbers(argv[i]) <= 0 || ft_atol(argv[i]) > MAX_PHILO_NUMS)
-		return (-1);
+		return (print_error_message(), -1);
+	if (valid_just_numbers(argv[i]) <= 0 || ft_atol(argv[i]) > 200)
+		return (print_error_message(), -1);
 	i++;
-	// Valida os timestamps em ms
 	while (i < argc)
 	{
 		if (valid_just_numbers(argv[i]) <= 0 || ft_atol(argv[i]) == -1)
-			return (-1);
+			return (print_error_message(), -1);
 		i++;
 	}
 	return (0);
