ce79845fb9967c02f58470a671bf47e9cf86af1e
GustavonBrito <gustavoserafio@gmail.com>
2025-05-25 21:41:07 -0300

    feat: adding enginefunction adapted to nomr

--- philo/philo_routine.c
+++ philo/philo_routine.c
@@ -6,21 +6,22 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 00:08:24 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/05/22 23:18:23 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/05/25 02:02:06 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// arquivo para criar uma funcao *void que faz toda a rotina de um filosofo, pensa, come, dorme e talvez morre.s
-
 #include "philo.h"
 
-void *philo_routine(void *arg)
+void	*philo_routine(void *arg)
 {
-	t_philo *philo = (t_philo *)arg;
+	t_philo	*philo;
 
+	philo = (t_philo *)arg;
 	while (1)
 	{
-		if (philo->rules->someone_died == 1 || (philo->meals == philo->rules->must_eat  && philo->rules->must_eat != 0))
+		if (philo->rules->someone_died == 1
+			|| (philo->meals == philo->rules->must_eat
+				&& philo->rules->must_eat != 0))
 			return (NULL);
 		think(philo);
 		eat(philo);
